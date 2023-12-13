cb1f56b63fdde7350862b78c47ccfa7307bdb4c9
PavelSmet <153732433+PavelSmet@users.noreply.github.com>
2023-12-13 19:20:22 +0700

    Update Freq_main.py
    Поменяли lib_write на Freq_lib в Freq_main. Изменили путь до папок

--- Freq_main.py
+++ Freq_main.py
@@ -1,10 +1,10 @@
-import lib_write
+import Freq_lib
 import os
 """
-text = lib_write.input_from_file() #чтение файла text.txt и чистим от лишней пунктуации. Функция возвращает строку слов, разделенных пробелом 
-lib_write.input_str(text) #принимает в качестве параметра строку, приводим к нижнему регистру, разбиваем на слова и записываем в словарь vocabulary.txt. Сделано ограничение по длине слова (от 3 символов)
-lib_write.prepare() #сортировка словаря в алфавитном порядке
-lib_write.spacedel() #удаление из словаря "переносы строки" (\n)
+text = Freq_lib.input_from_file() #чтение файла text.txt и чистим от лишней пунктуации. Функция возвращает строку слов, разделенных пробелом 
+Freq_lib.input_str(text) #принимает в качестве параметра строку, приводим к нижнему регистру, разбиваем на слова и записываем в словарь vocabulary.txt. Сделано ограничение по длине слова (от 3 символов)
+Freq_lib.prepare() #сортировка словаря в алфавитном порядке
+Freq_lib.spacedel() #удаление из словаря "переносы строки" (\n)
 """
 # Указываем путь к директории
 directory = "Vocabularys/"
@@ -16,8 +16,8 @@ files = os.listdir(directory)
 print(*files, sep='\n')
 
 Vocabulary_name = input("\nВведите имя словаря: ")
-sorted_dict = lib_write.vfreq(Vocabulary_name) #создание питоновского частотного словаря
-lib_write.vfreq_show(sorted_dict) #визуализация, вывод наиболее часто встречающихся слов
+sorted_dict = Freq_lib.vfreq(Vocabulary_name) #создание питоновского частотного словаря
+Freq_lib.vfreq_show(sorted_dict) #визуализация, вывод наиболее часто встречающихся слов
 
 
 #прикрутить индексы к словарям
